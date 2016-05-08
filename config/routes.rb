0d9f55058f19c00ef73ada65b2165e56b05eb8eb
skyhitblog <avijit@skyhitblog.com>
2016-05-09 02:38:43 +0530

    Clean up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 CarrierWaveExample::Application.routes.draw do
 
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
-  get "home/index"
+
   get "creatives/index"
+  root "creatives#index"
+
   devise_for :users, controllers: { registrations: "registrations" }
   resources :resumes, only: [:index, :new, :create, :destroy]
-  root "creatives#index"
+  
 end
